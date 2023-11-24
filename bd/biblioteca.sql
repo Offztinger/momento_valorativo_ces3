61bf84471a5629a12b855b58de21644c1a0f9bf8
Luis Alejandro Calvo Alvarez <offztinger@gmail.com>
2023-11-23 19:38:11 -0500

    Subiendo SQL con comandos para creacion de BD

---
+++ bd/biblioteca.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE BIBLIOTECA;
+USE BIBLIOTECA;
+
+CREATE TABLE `biblioteca`.`libros` (
+    `id_libro` INT NOT NULL,
+    `titulo_libro` VARCHAR(45) NOT NULL,
+    `autor_libro` VARCHAR(45) NOT NULL,
+    `paginas_libro` VARCHAR(45) NULL,
+    PRIMARY KEY (`id_libro`)
+);
