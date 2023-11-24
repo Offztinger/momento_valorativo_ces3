b78c2d34cddc784b2cbc14e100f5eeff6f1f301c
Luis Alejandro Calvo Alvarez <offztinger@gmail.com>
2023-11-23 20:06:43 -0500

    Subiendo conexion

---
+++ src/main/java/co/edu/elpoli/ces3/biblioteca_ces3/model/Connection.java
@@ -0,0 +1,42 @@
+package co.edu.elpoli.ces3.biblioteca_ces3.model;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class Connection {
+
+    // Librería de MySQL
+    private String driver = "com.mysql.jdbc.Driver";
+
+    // Nombre de la base de datos
+    private String database = "biblioteca";
+
+    // Host
+    private String hostname = "localhost";
+
+    // Puerto
+    private String port = "3306";
+
+    // Ruta de nuestra base de datos (desactivamos el uso de SSL con "?useSSL=false")
+    private String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=false";
+
+    // Nombre de usuario
+    public String username = "diego";
+
+    // Clave de usuario
+    public String password = "123";
+
+    public Connection(){
+
+    }
+
+    public java.sql.Connection getConexion(){
+        try {
+            Class.forName(driver);
+            return DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
