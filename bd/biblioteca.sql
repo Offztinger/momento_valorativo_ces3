5a0a156bc7e42f11c890a021ba95601580476212
Luis Alejandro Calvo Alvarez <offztinger@gmail.com>
2023-11-23 21:11:57 -0500

    Cambios en la sangria

--- bd/biblioteca.sql
+++ bd/biblioteca.sql
@@ -2,8 +2,8 @@ CREATE DATABASE BIBLIOTECA;
 USE BIBLIOTECA;
 
 CREATE TABLE `biblioteca`.`libros` (
-                                       `id` INT NOT NULL AUTO_INCREMENT,
-                                       `bookname` VARCHAR(45) NOT NULL,
-                                       `author` VARCHAR(45) NOT NULL,
-                                       PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `bookname` VARCHAR(45) NOT NULL,
+    `author` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`)
 );
