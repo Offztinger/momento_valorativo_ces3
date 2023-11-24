8538840b5cc3de38b286db3a788a4e61a51ae3d9
Luis Alejandro Calvo Alvarez <offztinger@gmail.com>
2023-11-23 20:46:02 -0500

    Subiendo cambios y controlador

---
+++ src/main/java/co/edu/elpoli/ces3/biblioteca_ces3/controller/CtrBook.java
@@ -0,0 +1,87 @@
+package co.edu.elpoli.ces3.biblioteca_ces3.controller;
+
+//import co.edu.elpoli.ces3.momentovalorativo.dto.DtoBook;
+//import co.edu.elpoli.ces3.momentovalorativo.model.Book;
+
+import co.edu.elpoli.ces3.biblioteca_ces3.dto.DtoBook;
+import co.edu.elpoli.ces3.biblioteca_ces3.model.Book;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CtrBook {
+
+    private Book modelBook;
+
+    public CtrBook(){
+        modelBook = new Book();
+    }
+
+    public DtoBook addBook(DtoBook Book){
+        try {
+            Book newBook = modelBook.create(Book);
+            return new DtoBook(newBook.getId(), newBook.getBookname(), newBook.getAuthor());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<DtoBook> getAllBooks() {
+        try {
+            ArrayList<Book> Books = modelBook.all(); // Llama al método 'all' del modelo
+            ArrayList<DtoBook> dtoBooks = new ArrayList<>();
+
+            for (Book Book : Books) {
+                DtoBook dtoBook = new DtoBook(
+                        Book.getId(),
+                        Book.getBookname(), //Se cambia por getBookname Document
+                        Book.getAuthor() //Se cambia por getAuthor Name
+                );
+                dtoBooks.add(dtoBook);
+            }
+
+            return dtoBooks;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public DtoBook getBookById(int BookId) {
+        try {
+            Book Book = modelBook.findById(BookId);
+            if (Book != null) {
+                return new DtoBook(Book.getId(), Book.getBookname(), Book.getAuthor());
+            } else {
+                throw new RuntimeException("No se encuentra el libro");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public DtoBook updateBook(int BookId, DtoBook updatedBook) {
+        try {
+            Book Book = new Book(
+                    BookId,
+                    updatedBook.getBookname(),
+                    updatedBook.getAuthor()
+
+            );
+
+            Book updated = modelBook.update(Book);
+            return new DtoBook(updated.getId(), updated.getBookname(), updated.getAuthor());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteBook(int BookId) {
+        try {
+            modelBook.delete(BookId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+   }
+}
+
+
+}
--- src/main/java/co/edu/elpoli/ces3/biblioteca_ces3/servlet/BookServlet.java
+++ src/main/java/co/edu/elpoli/ces3/biblioteca_ces3/servlet/BookServlet.java
@@ -1,4 +1,140 @@
 package co.edu.elpoli.ces3.biblioteca_ces3.servlet;
 
-public class BookServlet {
+import co.edu.elpoli.ces3.biblioteca_ces3.controller.CtrBook;
+import co.edu.elpoli.ces3.biblioteca_ces3.dto.DtoBook;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(name = "BookServlet", value = "/Book")
+public class BookServlet extends MyServlet{
+    private String message;
+
+    private GsonBuilder gsonBuilder;
+
+    private Gson gson;
+
+    private ArrayList<DtoBook> libros;
+
+    CtrBook ctr = new CtrBook();
+
+    public void init() {
+        gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
+        libros = new ArrayList<>();
+
+        DtoBook libro1 = new DtoBook();
+        //libro1.settitulo("Hamlet");
+        //libro1.setautor("William Shakespeare");
+
+        libros.add(libro1);
+
+        for (int i = 0; i < libros.size(); i++)
+        {
+            System.out.println(libros.get(i));
+        }
+        message = "Hi Lector";
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        resp.setContentType("application/json");
+        JsonObject body = this.getParamsFromPost(req);
+        DtoBook std = new DtoBook(
+                body.get("titulo").getAsString(),
+                body.get("autor").getAsString()
+        );
+
+        DtoBook newBook = ctr.addBook(std);
+
+        out.print(gson.toJson(newBook));
+        out.flush();
+
+
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        resp.setContentType("application/json");
+        String libroIdParam = req.getParameter("id");
+
+        if (libroIdParam != null && !libroIdParam.isEmpty()) {
+            int libroId = Integer.parseInt(libroIdParam);
+            DtoBook libro = ctr.getBookById(libroId);
+            out.print(gson.toJson(libro));
+        } else {
+            ArrayList<DtoBook> libros = ctr.getAllBooks();
+            out.print(gson.toJson(libros));
+        }
+
+        out.flush();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        resp.setContentType("application/json");
+        BufferedReader reader = req.getReader();
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+
+        JsonObject body = gson.fromJson(stringBuilder.toString(), JsonObject.class);
+        int libroId = body.get("id").getAsInt();
+
+        DtoBook updatedBook = new DtoBook(
+                body.get("titulo").getAsString(),
+                body.get("autor").getAsString()
+        );
+
+        DtoBook result = ctr.updateBook(libroId, updatedBook);
+
+        out.print(gson.toJson(result));
+        out.flush();
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        resp.setContentType("application/json");
+
+        int libroId = Integer.parseInt(req.getParameter("id"));
+
+        ctr.deleteBook(libroId);
+
+        out.print(gson.toJson("Book eliminado"));
+        out.flush();
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String method = req.getMethod();
+        switch (method){
+            case "PATCH":
+                this.doPatch(req, resp);
+                break;
+            default:
+                super.service(req, resp);
+        }
+    }
+
+    protected void doPatch(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("PATCH Method!");
+    }
+
 }
